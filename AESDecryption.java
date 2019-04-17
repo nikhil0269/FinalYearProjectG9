25fb001d5e70b984c00f4b3d759a380216560146
nikhil0269 <nikhil0269@gmail.com>
2019-04-17 11:33:10 +0530

    initial commit

---
+++ AESDecryption.java
@@ -0,0 +1,172 @@
+//package algorithms;
+import java.io.*;
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+import java.util.Random;
+
+public class AESDecryption {
+        public static String initVector = "dec07f6e-3b47-42";
+//        public static String key = "passwordpassword";
+        public static String getKey(String sessionId){
+            return sessionId.substring(0, 16);
+        }
+
+        public static String encrypt(String key, String initVector, String value) {
+            try {
+                IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+                SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+
+                Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+                cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
+
+                byte[] encrypted = cipher.doFinal(value.getBytes());
+                System.out.println("encrypted string: "+Base64.getEncoder().encodeToString(encrypted));
+
+                return Base64.getEncoder().encodeToString(encrypted);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            return null;
+        }
+
+        public static byte[] encrypt(String key, String initVector, byte[] value) {
+            try {
+                IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+                SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+
+                Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+                cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
+
+                byte[] encrypted = cipher.doFinal(value);
+
+                return encrypted;
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            return null;
+        }
+
+        public static String decrypt(String key, String initVector, String encrypted) {
+            try {
+                IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+                SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+
+                Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+                cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
+                byte[] original = cipher.doFinal(Base64.getDecoder().decode(encrypted));
+
+                return new String(original);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            return null;
+        }
+
+        public static byte[] decrypt(String key, String initVector, byte[] encrypted) {
+            try {
+                IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+                SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+
+                Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+                cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
+                byte[] original = cipher.doFinal(encrypted);
+
+                return original;
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            return null;
+        }
+
+        public static String realContent(String content){
+            return content.substring(content.indexOf(" ") + 1, content.length());
+        }
+
+        public static String getRandomKey() {
+            return getRandomStr(16);
+        }
+
+        public static String getRandomStr(int strLen){
+            char[] chars = "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxwz!@#$%^&*()=-".toCharArray();
+            StringBuilder sb = new StringBuilder();
+            Random random = new Random();
+            for (int i = 0; i < strLen; i++) {
+                char c = chars[random.nextInt(chars.length)];
+                sb.append(c);
+            }
+            return  sb.toString();
+        }
+
+
+
+    public String readFile(String file) throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader (file));
+    String         line = null;
+    StringBuilder  stringBuilder = new StringBuilder();
+    String         ls = System.getProperty("line.separator");
+
+    try {
+        while((line = reader.readLine()) != null) {
+            stringBuilder.append(line);
+            stringBuilder.append(ls);
+        }
+
+        return stringBuilder.toString();
+    } finally {
+        reader.close();
+    }
+}
+
+//write to the file
+
+public String writeFile(byte[] encrypted) throws IOException
+{
+
+
+
+ String FILEPATH = "encryptedFile"; 
+ File file = new File(FILEPATH); 
+
+	try {
+
+            // Initialize a pointer
+            // in file using OutputStream
+            OutputStream os = new FileOutputStream(file);
+
+            // Starts writing the bytes in it
+            os.write(encrypted);
+            //System.out.println("Successfully"
+              //                 + " byte inserted");
+
+            // Close the file
+            os.close();
+        }
+
+        catch (Exception e) {
+            System.out.println("Exception: " + e);
+        }
+
+return null;
+}
+
+	public static void main(String args[]) throws Exception
+	{
+		//------ AES
+        String input = "AES is strongest sync encryption algo";
+        //initialVector.length = key.length
+	AESDecryption a=new AESDecryption();
+	String file="encryptedFile";
+	input=a.readFile(file);
+	
+
+        String key = "1231231231231231";
+        String initialVector = "dect7f6e-3b47ge2";
+
+        //byte[] encrypted = AES.encrypt(key, initialVector, input.getBytes());
+	//a.writeFile(encrypted);
+        byte[] decrypted = AES.decrypt(key, initialVector, input.getBytes());
+       // System.out.println("AES test result: " + new String(decrypted));
+	}
+    }
---
+++ ThripleDESBase64.java
@@ -0,0 +1,79 @@
+//package algorithms;
+import java.io.*;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.MessageDigest;
+import java.util.Base64;
+
+public class ThripleDESBase64 {
+//        private static String key;
+        public static String encrypt(String base64Key, String plainText) throws Exception {
+            final MessageDigest md = MessageDigest.getInstance("md5");
+//            final byte[] digestOfPassword = md.digest("HG58YZ3CR9"
+//                    .getBytes("utf-8"));
+
+            final SecretKey key = new SecretKeySpec(
+                    Base64.getDecoder().decode(base64Key), "DESede");
+            final IvParameterSpec iv = new IvParameterSpec(new byte[8]);
+            final Cipher cipher = Cipher.getInstance("DESede/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+
+            final byte[] plainTextBytes = plainText.getBytes("utf-8");
+            final byte[] cipherText = cipher.doFinal(plainTextBytes);
+            String base64EncodedCipherText = Base64.getEncoder().encodeToString(cipherText);
+            return base64EncodedCipherText;
+        }
+
+        public static String decrypt(String base64Key, String base64CipherText) throws Exception {
+            byte[] cipherbytes = Base64.getDecoder().decode(base64CipherText);
+            final MessageDigest md = MessageDigest.getInstance("md5");
+//            ede
+            final SecretKey key = new SecretKeySpec(
+                    Base64.getDecoder().decode(base64Key),
+                    "DESede"
+            );
+//            final IvParameterSpec iv = new IvParameterSpec(new byte[8]);
+            final Cipher decipher = Cipher.getInstance("DESede/ECB/PKCS5Padding");
+//            decipher.init(Cipher.DECRYPT_MODE, key, iv);
+            decipher.init(Cipher.DECRYPT_MODE, key);
+            final byte[] plainText = decipher.doFinal(cipherbytes);
+
+            return new String(plainText, "UTF-8");
+        }
+
+
+
+
+    public String readFile(String file) throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader (file));
+    String         line = null;
+    StringBuilder  stringBuilder = new StringBuilder();
+    String         ls = System.getProperty("line.separator");
+
+    try {
+        while((line = reader.readLine()) != null) {
+            stringBuilder.append(line);
+            stringBuilder.append(ls);
+        }
+
+        return stringBuilder.toString();
+    } finally {
+        reader.close();
+    }
+}
+
+
+
+	public static void main(String args[]) throws Exception
+	{
+	ThripleDESBase64 a=new ThripleDESBase64();	
+	String input = "3DES is another strong sync encryption algo";
+        String DESbase64Key = "6lRqh2yaYyc1NV2oCcqltC0hYp/9Lnn5";
+	input=a.readFile("5mb.txt");
+        String encryptedBase63_3DES = ThripleDESBase64.encrypt(DESbase64Key, input);
+        //String decrypted3DES = ThripleDESBase64.decrypt(DESbase64Key, encryptedBase63_3DES);
+        //System.out.println("3DES test result:" + decrypted3DES);
+	}
+    }
